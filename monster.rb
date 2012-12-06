98ae5eda1e6867beb9e1891b92cb1dc4ccaad270
Erik Ivares <erik.ivares@elev.it-gymnasiet.se>
2012-12-06 15:48:30 +0100

    added skin apearence and finiched hunger and mood in descripition

--- monster.rb
+++ monster.rb
@@ -1,11 +1,31 @@
-class Moster
+class Monster
 
-	def initialize (age, hunger, mood)
-		@name = gets.chomp
+	
+	def initialize 
+		@colors = ["blue", "purple", "red", "orange", "yellow", "green"]
+		@moods = ["angry", "sleepy", "restless", "happy", "joyfull"]
+		@hunger_levels = ["starving", "hungry", "not hungry", "full"]
+		@skins = ["scaly", "lethery", "fury", "stone hard", "bloby"]
+		@skin = @skins[rand(@skins.size)]
+		@color = @colors[rand(@colors.size)]
+		@name = gets.chomp.downcase.capitalize
+		age = rand(1-50)
+		@age = age
+		hunger = 5
 		@hunger = hunger
+		mood = 1
 		@mood = mood
 	end
 
-	
+	def description
+		return "#{@name} is a monster at the age of #{@age}, he have #{@color} skin and a #{@skin} apperence"
+	end
+
 
 end
+
+puts "please enter the name of your new monster"
+
+monster = Monster.new
+
+puts monster.description
