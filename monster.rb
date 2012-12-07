0df536e08d87c7bcdf55010db05b334911cf03e4
Erik Ivares <erik.ivares@elev.it-gymnasiet.se>
2012-12-07 08:16:39 +0100

    added the possebility to do thing with the monster
    you get a choise of 5 thing todo XD

--- monster.rb
+++ monster.rb
@@ -6,6 +6,8 @@ class Monster
 		@moods = ["angry", "sleepy", "restless", "happy", "joyfull"]
 		@hunger_levels = ["starving", "hungry", "not hungry", "full"]
 		@skins = ["scaly", "lethery", "fury", "stone hard", "bloby"]
+		@cities = ["Tokyo", "Tokyo", "Stockholm", "London", "Washinton", "Area 51"]
+		@city = @cities[rand(@cities.size)]
 		@skin = @skins[rand(@skins.size)]
 		@color = @colors[rand(@colors.size)]
 		@name = gets.chomp.downcase.capitalize
@@ -21,7 +23,35 @@ class Monster
 		return "#{@name} is a monster at the age of #{@age}, he have #{@color} skin and a #{@skin} apperence"
 	end
 
-
+	def menu
+		todo = 0
+		until todo == 5
+			puts "what whould you whant your monster to do?
+				1. Eat something
+				2. Destroy a building
+				3. Play a game
+				4. Eliminate a city (monster may die)
+				5. say goodbye"
+			todo = gets.to_i
+			if todo == 1
+				puts "#{@name} ate a coockie"
+			elsif todo == 2
+				puts "the building had no chance against #{@name}"
+			elsif todo == 3 
+				puts "#{@name} played russian roulette with his friends (it's a trick he knows where the bullet is in the gun)"
+			elsif todo == 4
+				die = rand(100)
+				if die == 69
+					puts "#{@name} died in combat" 
+					todo = 5
+				else
+					puts "#{@name} succesfully destoyed #{@city}"
+				end
+			elsif todo == 5
+				exit
+			end
+		end
+	end
 end
 
 puts "please enter the name of your new monster"
@@ -29,3 +59,4 @@ puts "please enter the name of your new monster"
 monster = Monster.new
 
 puts monster.description
+puts monster.menu
